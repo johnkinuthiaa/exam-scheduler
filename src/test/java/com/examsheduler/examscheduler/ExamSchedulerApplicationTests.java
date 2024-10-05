b42c6e47bd352a764f4c423f784f63e477a6b599
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-05 17:28:29 +0300

    readme file

---
+++ src/test/java/com/examsheduler/examscheduler/ExamSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examsheduler.examscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
